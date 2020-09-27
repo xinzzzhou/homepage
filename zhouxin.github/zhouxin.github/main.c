e49c1d2865a4498ee75343f6031e3022fa2e515f
zhouxin <xxxcaixiner@gmail.com>
2020-09-27 20:37:47 +0800

    test

---
+++ zhouxin.github/zhouxin.github/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  zhouxin.github
+//
+//  Created by 周馨 on 2020/9/27.
+//  Copyright © 2020年 ZhouXin. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
